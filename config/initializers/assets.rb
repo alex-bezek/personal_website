7519972209d0bfcd4d1bb4ef9529ce7a2df07985
Alex Bezek <alex.bezek@cerner.com>
2016-10-17 22:37:33 -0500

    webpack being used for the asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,4 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
-# Add additional assets to the asset load path when asset precompiling production assets
-Rails.application.config.assets.paths = [Rails.root.join('tmp', 'assets')] if Rails.env.production?
+Rails.application.config.assets.paths = [Rails.root.join('tmp', 'assets')]
